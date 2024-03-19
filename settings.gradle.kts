1d28357cc0582b5267fcdae9ffd3a9405cadf731
Kochkin Ivanov <ivanovkochkin@iMac-Administrator.local>
2024-03-19 15:01:45 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "smooth_criminal_michael_jackson_sony_picture_production_edit"
+include(":app")
